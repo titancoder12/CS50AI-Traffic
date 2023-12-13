03cbee794cf54f10d1e982a396618fe94f1e5749
Christopher Lin <baby.titan.lin@gmail.com>
2023-12-13 12:33:51 -0800

    Found a better combination with accuracy 0.9277 and loss 0.2652

--- traffic.py
+++ traffic.py
@@ -90,30 +90,20 @@ def get_model():
     """
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(
-            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            128, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
 
-        tf.keras.layers.Conv2D(
-            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-        ),
-
         tf.keras.layers.Flatten(),
 
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
-
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
-
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
-
         tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
 
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation="relu")
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
     model.compile(
         optimizer = "adam",
