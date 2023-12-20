3ed639b7deabfc5effe8a0f2424ffe21e4a1b4f3
Christopher Lin <baby.titan.lin@gmail.com>
2023-12-20 12:59:29 -0800

    relatively good, with loss 0.0583 and accuracy 0.9178

--- traffic.py
+++ traffic.py
@@ -65,7 +65,7 @@ def load_data(data_dir):
     images = []
     labels = []
     for i in range(NUM_CATEGORIES):
-        print(i)
+        #print(i)
         os.chdir(str(i))
         #img_group = []
         #print(len(os.listdir(os.getcwd())))
@@ -90,23 +90,23 @@ def get_model():
     """
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(
-            128, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
 
         tf.keras.layers.Flatten(),
 
-        tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dense(64, activation="relu"),
         tf.keras.layers.Dense(64, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.1),
 
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
     model.compile(
         optimizer = "adam",
-        loss = "categorical_crossentropy",
+        loss = "categorical_focal_crossentropy",
         metrics=["accuracy"],
     )
     return model
